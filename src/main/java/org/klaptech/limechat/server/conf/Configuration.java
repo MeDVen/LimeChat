9079af09a7150dc66eb27dd55fb1303cbc557ec8
rlapin <stioneq@gmail.com>
2015-08-22 18:47:26 +0300

    Add  - javadoc

--- src/main/java/org/klaptech/limechat/server/conf/Configuration.java
+++ src/main/java/org/klaptech/limechat/server/conf/Configuration.java
@@ -1,8 +1,13 @@
 package org.klaptech.limechat.server.conf;
 
 /**
+ * Configuration for server
  * @author rlapin
  */
 public interface Configuration {
+    /**
+     * get listen port
+     * @return listen port , where clients should connect
+     */
     int getPort();
 }
