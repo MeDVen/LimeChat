c9300ec0368ae91d659b538a56c49e367961eb54
MeDVen <darkvenomx@gmail.com>
2015-09-20 16:21:28 +0300

    MembersView add with testData.

---
+++ src/main/java/org/klaptech/limechat/client/gui/components/chatroom/MembersView.java
@@ -0,0 +1,54 @@
+package org.klaptech.limechat.client.gui.components.chatroom;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Group;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import org.klaptech.limechat.client.entities.UserInfo;
+import org.klaptech.limechat.shared.enums.UserState;
+
+/**
+ * View of members in room. Display members name and status circle.
+ *
+ * <ul>Circle colors:
+ * <li>Green - online</li>
+ * <li>White - offline</li>
+ * <li>Yellow - afk</li>
+ * <li>Red - busy</li>
+ * </ul>
+ *
+ * @see UserState
+ *
+ * @author MeDVen
+ */
+public class MembersView extends Group {
+
+    private ListView<UserInfo> listView;
+
+    private ObservableList<UserInfo> testData = FXCollections.observableArrayList(new UserInfo("user1", UserState.ONLINE),
+            new UserInfo("user2", UserState.AFK), new UserInfo("user3", UserState.BUSY), new UserInfo("user4", UserState.OFFLINE));
+
+    public MembersView() {
+        getStylesheets().add(getClass().getClassLoader().getResource("fxml/membersview.css").toExternalForm());
+        initComponents();
+    }
+
+    private void initComponents() {
+        listView = new ListView<>();
+        listView.setItems(testData);
+        listView.setCellFactory(param -> new UserCell());
+        getChildren().add(listView);
+    }
+
+    // TODO add colored circle-statuses
+    private class UserCell extends ListCell<UserInfo> {
+        public void updateItem(UserInfo item, boolean empty) {
+            super.updateItem(item, empty);
+            if (item != null) {
+                setText(item.getName() + " " + item.getState().toString());
+            }
+        }
+    }
+
+}
